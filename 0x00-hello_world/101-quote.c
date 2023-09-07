95a512e745fb723d39572c5c93343652032845b0
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-07 05:49:01 -0700

    print with fput

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  *main-Entry point
  *
- *Description: A C program that prints with put function
- *
+ *Description: print a qoute using write function
+ *ssize_t write(int fd, const void *buf, size_t count);
  *Return: Always 1(Success)
  */
 
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
