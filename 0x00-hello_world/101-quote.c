8d2e972e63a70bef6c32aac203f5acd8481bf594
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-07 05:35:26 -0700

    print with fput

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Always 1(Success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
