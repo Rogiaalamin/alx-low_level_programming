4d9c39c3e9c83b24f38b022b56e5887ac440a385
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-07 05:40:16 -0700

    print with fput

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -10,6 +11,7 @@
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
