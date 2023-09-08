d5fc98cf65004acf3aab98767bd8b5abf37d0db7
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-08 11:20:21 -0700

    combination of brains and materials.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: A C program that prints numbers from 0 to 9
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+        int n, i;
+
+        while (n >= 0 && n < 10)
+        {
+		while (i >= 1 && n < 10)
+                	putchar(n + '0');
+			putchat(i + '0');
+                	if (i != 9)
+                	{
+                        	putchar(',');
+                        	putchar(' ');
+                	}
+                n++;
+        }
+        putchar('\n');
+        return (0);
+}
