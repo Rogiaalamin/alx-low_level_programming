9196b221c2e8fbd443529f74a45ccfdd40b19e45
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-08 10:54:27 -0700

    numers sperate by ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: A C program that prints numbers from 0 to 9
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	while (n >= 0 && n < 10)
+	{
+		putchar(n + '0', ',');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
