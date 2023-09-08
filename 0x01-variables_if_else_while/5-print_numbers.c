54c7806404907931e9c80dc15ec91127e714ae73
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-08 05:46:52 -0700

    numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: A C program that prints numbers from 0 to 9
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	if (n >= 0 && n < 10)
+	{
+		printf("%d \n", n);
+	}
+	return (0);
+}
