4595a118cd2700a4aafebc730533e458df6c6cea
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-08 11:23:33 -0700

    combination of brains and materials.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
         {
 		while (i >= 1 && i < 10)
                 	putchar(n + '0');
-			putchat(i + '0');
+			putchar(i + '0');
                 	if (i != 9)
                 	{
                         	putchar(',');
