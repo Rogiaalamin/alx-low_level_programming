94830bf49e453ad4dac525ccd9dc366ec1a54d16
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-08 19:21:55 -0700

    combinations of three digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: A C program that prints numbers from 0 to 9
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+
+	while (i <= 9)
+	{
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(i + '0');
+			putchar(' ');
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
