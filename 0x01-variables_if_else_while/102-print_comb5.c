dd14e4f81df98778a872ea874e89181f72ef8731
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-08 19:33:59 -0700

    combinations of three digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,28 +10,30 @@
 
 int main(void)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j;
 
-	while (i <= 98)
+	while (i <= 99)
 	{
+		i = j;
 		while (j <= 99)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-
-			if (i != 98 || j != 99)
+			if (j != 0)
 			{
-				putchar(',');
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
 				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i != 99 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			j++;
 		}
 		i++;
-		j = 0;
 	}
 	putchar('\n');
 	return (0);
