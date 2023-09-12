b6a81761deef1f54521807d94e605ee39fdea219
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-12 11:17:34 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * _putchar A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char c[] = {"_putchar"};
+
+	for (i = 0; i < 9; i++)
+	_putchar(c[i]);
+
+	return (0);
+}
