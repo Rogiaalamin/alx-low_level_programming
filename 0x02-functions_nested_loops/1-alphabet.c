5a1adff266f86a8fdc51201ebaaa5b29b2aa4c23
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-12 10:25:54 -0700

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
  * print_alphabet it is a function to print the alphabet a - z
- * 
- * Reyurn always 0
+ * Return always 0
  */
 
 void print_alphabet(void)
-{
 	char c = 'a';
 
 	while (c <= 'z')
@@ -15,4 +13,3 @@ void print_alphabet(void)
 		c++;
 	}
 	_putchar('\n');
-}
