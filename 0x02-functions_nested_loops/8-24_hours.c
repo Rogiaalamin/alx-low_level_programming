921e5f640bfc8386bb4a02179547c50940416d54
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-13 03:59:37 -0700

    hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - A function that check code
+ *
+ * Return: return 0 (Success)
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i);
+			_putchar(j);
+			_putchar(':');
+			_putchar(i);
+		}
+			_putchar(j);
+			_putchar('\n');
+	}
+}
