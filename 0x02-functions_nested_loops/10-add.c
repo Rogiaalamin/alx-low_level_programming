d70b0d4840bb622d5620d1ccc47cdda082dc9a5b
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-13 09:01:57 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add  - A function that check code
+ *
+ * @a: is a parmeter of function
+ * @b: is a parmeter of function
+ *
+ * Return: return 0 (Success)
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
