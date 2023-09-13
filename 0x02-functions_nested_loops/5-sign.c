d8d152a417dd985ec7d64539bcd7d6b5472b8936
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-13 01:10:09 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - A function that check code
+ *
+ * @n: is a parmeter of function
+ *
+ *Return:return 1 if 'n' postive or 0 if 0 else return -1 if negative (Success)
+ *
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		-putchar("+, ");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0, ");
+		return (0);
+	}
+	else
+	{
+		_putchar("-, ");
+		return (-1);
+	}
+}
