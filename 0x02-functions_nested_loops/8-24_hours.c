dccf79221a7cb7d9652f334cb483fbb0eb105f2b
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-13 04:05:03 -0700

    hour

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,13 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 9)
 		{
 			_putchar(i);
 			_putchar(j);
 			_putchar(':');
 			_putchar(i);
-		}
+			j++;
 			_putchar(j);
 			_putchar('\n');
 	}
