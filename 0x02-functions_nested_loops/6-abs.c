bd47e03c94f6f273973c9afac3f44b19b6d1a71c
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-13 01:37:41 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - A function that check code
+ *
+ * @c: is a parmeter of function
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int _abs(int)
+{
+
+	if (c < 0)
+		c = c * '-';
+		_putchar('c');
+	else
+		_putchar('c');
+}
