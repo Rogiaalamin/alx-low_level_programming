3e25bce0b04b6ad446be80858bb5aa5f2756913c
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-13 09:09:36 -0700

    numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - A function that check code
+ *
+ * @n: is a parmeter of function
+ *
+ * Return: return 1 if 'c' lowerecase else return 0 (Success)
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = 0; i <= 98; i++)
+		return (i);
+}
