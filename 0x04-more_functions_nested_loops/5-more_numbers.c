f94bcd411b72919871026c65fa3adf06a845050e
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-17 04:03:29 -0700

    10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 	char c[] = "01234567891011121314";
 
 	for (i = 0; i < 10; i++)
-		for (j = 0; j > 21; j++)
+		for (j = 0; j < 21; j++)
 		{
 			_putchar(c[j]);
 			_putchar('\n');
