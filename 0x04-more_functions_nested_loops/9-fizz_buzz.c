3035506cb850a95e34ed69f6f40b96130fbd9cc1
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-17 08:39:44 -0700

    Fizz & Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,10 @@
 /**
  * main - check the code
  *
- * Description: program that prints the numbers from 1 to 100 unless %3&%5 !=0
+ * Description: program that prints the numbers 1 -100   followed by a new line
+ * multiples of three print Fizz instead of the number
+ * the multiples of five print Buzz
+ * For numbers which are multiples of both three and five print FizzBuzz
  *
  * Return: Always 0 (Success).
  */
