d72f1405c2cfc74a2b1dc63dc4cc8f01e5e6e61b
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-17 04:30:06 -0700

    15 digit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,16 @@
 void more_numbers(void)
 {
 	int i, j;
-	char c[] = "01234567891011121314";
 
 	for (i = 0; i < 10; i++)
-		for (j = 0; j < 21; j++)
+	{
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(c[j]);
-			_putchar('\n');
+			if (j < 10)
+				_putchar('0' + j);
+			else
+				_putchar('1' + (j-10));
 		}
 		_putchar('\n');
+	}
 }
