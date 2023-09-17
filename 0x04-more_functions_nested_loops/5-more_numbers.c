be3d3346ea9a238bff26bf794ae573a614f2b1cc
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-17 03:58:56 -0700

    10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void);
+{
+	int i, j;
+	char c[] = "01234567891011121314";
+
+	for (i = 0; i < 10; i++)
+		for (j = 0; j > 21; j++)
+		{
+			_putchar(c[j]);
+			_putchar('\n');
+		}
+		_putchar('\n');
+}
