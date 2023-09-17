becb75c3f6984ab1c61a6924176f8fbb1a500156
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-17 06:03:36 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code
+ *
+ * @n: parameter of fuction
+ *
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
+
+
+
+
