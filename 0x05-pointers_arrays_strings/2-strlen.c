db8acd9ba32d468dd1fbfc97426e627315fbb06d
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-18 05:06:00 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen- check the code
+ *
+ * @s: is a parameter
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+		return (i);
+}
