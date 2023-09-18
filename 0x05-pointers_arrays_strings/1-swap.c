d60d531c414299e3119b605f9fffce662e9d0b2b
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-18 04:58:54 -0700

    pointer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,15 +4,14 @@
  *
  * @a: is a parametr 1
  * @b: is a parameter 2
- * @c: is a parameter 3
  *
  * Return: Always 0.
  */
 
 void swap_int(int *a, int *b)
 {
-	int c = *a;
+	int temp = *a;
 
 	*a = *b;
-	*b = c;
+	*b = temp;
 }
