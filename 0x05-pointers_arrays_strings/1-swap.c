41c975f297a65804fd31909473c4c7c82c5df83f
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-18 04:36:39 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ *
+ * @a: is a parametr 1
+ * @b: is a parameter 2
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
