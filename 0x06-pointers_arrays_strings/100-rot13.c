f90a3695433d1fe4b48f3a38cb2500524ff53267
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-09-21 02:55:42 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - check the code
+ *@s: string
+ * Return: Always 0.
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		str++;
+	}
+	return (ptr);
+}
