80ca498fc0c02f274a09943f9539d37a5437c8d8
Ruqaya Alamin <rogiaamin97@gmail.com>
2023-10-07 09:15:04 +0400

    arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include "main.h"
 
-/*main-print  check code.
- * @argc:int
- * @argv: list
- * Return: 0, non-zero-fail
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
+ * Return: Always 0 (Success)
  */
 
-int main (int argc, char *argv[] _attribute_((unsed)))
+int main(int argc, char *argv[])
 {
+(void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
+
 	return (0);
 }
-
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include "main.h"
 
-/*main - check code.
- * @argc:int
- * @argv: list
- * Return: 0
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
+ * Return: Always 0 (Success)
  */
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
 
-	while (argc--)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
-		i++;
 	}
+
 	return (0);
 }
+
