d1b140425ee7c16794b39741881c1fe7a899e27d
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-10-10 06:28:17 -0700

    structures_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - a dog struct
+ * @d: dog init
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = agee;
+		d->owner = owner;
+	}
+}
+
