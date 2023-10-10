dd0da1ee44d02638beb3293395e9037b8d81859e
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-10-10 05:58:44 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,14 @@
-#include <stdio.h>
+#ifdef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Descreption: just a lone dog syruct
+ */
 
 struct dog 
 {
@@ -6,3 +16,5 @@ struct dog
 	float age;
 	char* owner;
 };
+
+#endif
