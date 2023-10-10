7b28f2baa47d3b9ca8b854127cf637508181862e
Ruqaya Alamin <rogiaalamin97@gmail.com>
2023-10-10 04:42:28 -0700

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTION_LINK_MACRO_H
 #define _FUNCTION_LINK_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (-(x)) ;(x))
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif /*  _FUNCTION_LINK_MACRO_ */
 
